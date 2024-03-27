2414a54277385c97d604d0d1edcc79eb8de12fca
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-27 16:29:04 +0900

    Title: 다양하게 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240327/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    std::cout << "Total days in Year"<<"\n";
+    std::cout << "365"<<"\n";
+    std::cout << "Circumference rate"<<"\n";
+    std::cout << "3.1415926535";
+    return 0;
+}
