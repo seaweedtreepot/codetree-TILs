aaf676f02c2d4f494d4e56b6b4423febb88d787c
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:26:25 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    cout << fixed;
+    cout.precision(1);
+    cin >> a;
+
+    cout << a * 30.48;
+    return 0;
+}
