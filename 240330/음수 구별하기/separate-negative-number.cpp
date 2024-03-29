e56c619bb560ef53279dc3b74d61b8e74cef9b95
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 08:59:29 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240330/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a= 0;
+    cin >> a;
+    cout << a << endl;
+    if(a < 0)
+        cout << "minus" ;
+    return 0;
+}
