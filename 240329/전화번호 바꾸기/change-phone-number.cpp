7c649ab46d65f455f1ad23551aee294c0e7b13ab
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:59:13 +0900

    Title: 전화번호 바꾸기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240329/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int zero,front, rear;
+    cin >> zero;
+    cin.get();
+    cin >> front;
+    cin.get();
+    cin >> rear;
+    cout <<zero << "-" << rear << "-" << front;
+    return 0;
+}
