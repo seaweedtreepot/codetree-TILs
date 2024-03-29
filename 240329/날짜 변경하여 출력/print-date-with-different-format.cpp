84cddc5d3a82297685e377ded6882078c6bf6a7c
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:49:33 +0900

    Title: 날짜 변경하여 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/날짜 변경하여 출력/print-date-with-different-format.cpp
+++ 240329/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -4,8 +4,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int year, month, day;
     cin >> year ; cin.get();
-    cin >> month ; con.get();
-    cin > day;
+    cin >> month ; cin.get();
+    cin >> day;
 
     cout << month << "-" << day << "-" << year;
     return 0;
