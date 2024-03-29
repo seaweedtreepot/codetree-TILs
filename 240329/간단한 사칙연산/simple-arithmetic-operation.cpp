5f18b6079f0a4acb8c5f202d34a02e523945433b
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 10:21:27 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/간단한 사칙연산/simple-arithmetic-operation.cpp
+++ 240329/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -4,6 +4,6 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a ,b ;
     cin >> a >> b;
-    cout << a + b << endl << a - 5 << endl << a/b << endl << a%b;
+    cout << a + b << endl << a - b << endl << a/b << endl << a%b;
     return 0;
 }
