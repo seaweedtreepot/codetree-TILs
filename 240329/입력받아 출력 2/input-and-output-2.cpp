946fbc63b9c188a00a4945ee7251d56e4785871f
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:46:53 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int front_degits, rear_degits;
+    cin >> front_degits; cin.get();
+    cin >> rear_degits;
+    cout << front_degits << rear_degits;
+    return 0;
+}
