8927797fb22143b6a5cc645c9b74ae15feff29dc
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:48:55 +0900

    Title: 날짜 변경하여 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240329/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int year, month, day;
+    cin >> year ; cin.get();
+    cin >> month ; con.get();
+    cin > day;
+
+    cout << month << "-" << day << "-" << year;
+    return 0;
+}
