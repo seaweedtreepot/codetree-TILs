2fbb3f7dfce94caa3aa7ff484f8a3bb06684e2fa
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:39:51 +0900

    Title: 실수와 문자 받아 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b;
+    char c;
+    cout << fixed;
+    cout.precision(2);
+    cin >> c >> a >> b;
+    cout << c << "\n" << a << "\n" << b; 
+    return 0;
+}
