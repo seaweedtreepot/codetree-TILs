b9b5bfe00342843575e31be0b8d2b63cb7c850d1
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:25:08 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 0;
+    cout << fixed;
+    cout.precision(2);
+    cin >> a;
+    cout << a;
+    return 0;
+}
