078ef116bad90a9aa69451d3348d0825f5e36243
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-29 09:35:18 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240329/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    cout.precision(3);
+    double a, b ,c;
+    cin >> a >> b >> c;
+    cout << a << "\n" << b << "\n"  << c;
+    return 0;
+}
