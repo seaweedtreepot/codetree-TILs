2b83bf20cde3bfd2f7153f4ee866935009899eb3
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 09:58:01 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240330/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a =0;
+    cin >> a;
+
+    if(a%3==0){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+
+    if(a%5 == 0){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+    return 0;
+}
