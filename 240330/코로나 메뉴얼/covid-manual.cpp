2bd9f2a6274ca63918c3dca03cac42ad38ab5caf
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 11:14:33 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240330/코로나 메뉴얼/covid-manual.cpp
+++ 240330/코로나 메뉴얼/covid-manual.cpp
@@ -4,24 +4,23 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int counter,a_ther,b_ther,c_ther;
     char a,b,c;
-    counter =0;
     cin >> a >> a_ther;
     cin >> b >> b_ther;
     cin >> c >> c_ther;
-    if(a == 'Y'&& a_ther >=37){
-        counter++;
+
+    if(a == 'Y' && a_ther >= 37){
+        if((b=='Y'&&b_ther>=37)||(c=='Y'&&c_ther>=37)){
+            cout << "E";
+        }
+        else
+            cout << "N";
     }
-
-    if(b=='Y'&& b_ther >= 37)
-        counter++;
-    if(c=='Y'&&c_ther >= 37)
-        counter++;
-
-
-    if(counter>=2){
-        cout << "E";
+    else{
+        if((b=='Y'&&b_ther>=37)&&(c=='Y'&&c_ther>=37)){
+            cout << "E";
+        }
+        else
+            cout << "N";
     }
-    else 
-    cout << "N";
     return 0;
 }
