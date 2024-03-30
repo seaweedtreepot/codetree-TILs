d8fd6471ffa9bf0e027d8995da433d7ca8ccda30
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 10:04:14 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240330/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b ,c ,d ,e; cin >> a >> b >> c>> d >> e;
+    cout << (a > b)  << endl << (a > c) << endl << (a > d) 
+    << endl << (a > e) ;
+    return 0;
+}
