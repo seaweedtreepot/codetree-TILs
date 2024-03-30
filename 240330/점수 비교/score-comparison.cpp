1cfa1edd1051ee2c7ac3624a0f4f19ec933a0782
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 10:06:16 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240330/점수 비교/score-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_math, a_eng , b_math, b_eng;
+    cin >> a_math >> a_eng >> b_math >> b_eng;
+
+
+    cout << (a_math > b_math) && (a_eng>b_eng);
+    return 0;
+}
