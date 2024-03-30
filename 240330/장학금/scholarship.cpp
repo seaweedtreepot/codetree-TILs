dec85905013dae60cc4df8726e089049ff2fdc60
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 10:39:36 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240330/장학금/scholarship.cpp
+++ 240330/장학금/scholarship.cpp
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int mid_score,fin_score;
     cin >> mid_score >> fin_score;
-    if(mid_score < 90 && fin_score < 90){
+    if(mid_score < 90 || fin_score < 90){
         cout << 0;
     }
     else if( fin_score >=95){
