77cd9d726800c8a43e77634ed33dad3eac9eef4c
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 09:26:56 +0900

    Title: 살 수 있는 물건, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240330/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> b;
+
+    if(money >= 3000){
+        cout << "book";
+    }
+    else if (money >= 1000){
+        cout << "mask";
+    }else{
+        cout << "no";
+    }
+    return 0;
+}
