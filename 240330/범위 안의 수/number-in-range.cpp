8432866a9bdb94f2ba20c7d40f9ebcb834832326
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 10:11:48 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240330/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+
+    if(10 <= a && a <= 20){
+        cout <<"yes";
+    }
+    else{
+        cout << "no";
+    }
+    return 0;
+}
