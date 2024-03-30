720de502d855aba4e87a200880469cb376c16ac1
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-30 10:48:59 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240330/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int gen, age;
+    cin >> gen >> age;
+
+
+    if(gen == 0){
+        if(age>=19){
+            cout << "MAN";
+        }
+        else{
+            cout << "BOY";
+        }
+    }else {
+        if(age>=19){
+            cout <<"WOMAN";
+        }else{
+            cout << "GIRL";
+        }
+    }
+    return 0;
+}
