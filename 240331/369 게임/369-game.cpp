9ed759ff3a0716296fbceacf9669843f4c70445f
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-31 13:15:14 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240331/369 게임/369-game.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >>n;
+
+    for(int i = 1; i<=n;i++){
+        if(i%3==0){
+            cout << 0 <<" ";
+        }
+        else if (i%10%3 == 0){
+            cout << 0 << " ";
+        }else if(i/10!=0 && i/10%3==0){
+            cout << 0 << " ";
+        }else{
+            cout << i << " ";
+        }
+
+    }
+    return 0;
+}
