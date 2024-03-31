aa2258566c034913bbdc38ae29e5adec2195a74d
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-31 12:25:58 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240331/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int i = 0;
+    while(i<=n){
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
