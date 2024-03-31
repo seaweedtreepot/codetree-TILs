2134056ae66932a40fb56481c0e800374380ff60
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-31 12:35:47 +0900

    Title: 짝수만 출력 2, Time: 1080ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240331/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> b >> a;
+    while(b>=a){
+        if(b%2==0){
+            cout << b-- << " ";
+        }
+    }
+    return 0;
+}
