7e7ace1a17c0d31f3262acc88e638d6764ef95fd
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-31 13:37:16 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240331/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int mul_t=0, mul_f=0;
+    int num;
+    for(int i = 0 ; i < 10; i++){
+        cin >> num;
+        if(num%3==0 && num%5==0){
+            mul_t++; mul_f++;
+        }
+        else if(num%3){
+            mul_t++;
+        }else if(num%5){
+            mul_f++;
+        }
+    }
+    cout << mul_t << " " << mul_f;
+    return 0;
+}
