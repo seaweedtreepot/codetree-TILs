79f5f7c0f478d78bc4423fffd9143ae2e0c11c09
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-31 12:52:10 +0900

    Title: n번 더하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240331/n번 더하기/add-n-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a , n;
+    cin >> a>> n;
+    a+=n;
+    for(int i = 0; i < n; i++){
+        cout << a << "\n";
+        a+=n;
+    }
+    return 0;
+}
