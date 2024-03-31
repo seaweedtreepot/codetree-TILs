ddade93d8ac176c7f69144b52dd9c2b576ecc1e3
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-31 11:25:04 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240331/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+
+    for(int i= a; i < 101; i++){
+        cout << i << " ";
+    }
+    return 0;
+}
