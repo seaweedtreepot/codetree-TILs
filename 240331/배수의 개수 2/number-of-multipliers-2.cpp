5bfc59c354175785c3c7d0d3339b5b370664f2d6
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-03-31 13:33:10 +0900

    Title: 배수의 개수 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240331/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt =0;
+    for(int i = 0 ; i < 10 ; i++){
+        cin >> n;
+        if(n%2==1){
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
