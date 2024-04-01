8a487d9632e69b5ec39cb1f4b084b1a66f3782ec
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-01 16:18:40 +0900

    Title: a부터 b까지 짝수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240401/a부터 b까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a ,b;
+    int sum_val=0;
+    cin >> a >> b;
+
+    for(int i = a; i <= b; i++){
+        if(i%2==0){
+            sum_val += i;
+        }
+    }
+
+    cout << sum_val ;
+    return 0;
+}
