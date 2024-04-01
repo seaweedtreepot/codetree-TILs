41d33fd49fdbf5dd27ad57676eddfb705d7a0e3a
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-01 16:21:18 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240401/범위 안의 수 2/number-in-range-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum_val=0;
+    int cnt=0;
+    int n;
+
+    for(int i = 0 ; i < 10; i++){
+        cin >> n;
+        if(n>=0 && n<=200){
+            sum_val += n;
+            cnt++;
+        }
+        
+    }
+
+    cout << sum_val << " " <<  (double)sum_val/cnt;
+    return 0;
+}
