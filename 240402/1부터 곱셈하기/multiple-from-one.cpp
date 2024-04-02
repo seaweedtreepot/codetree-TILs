b0da0f21d5169580c1188bfa49ece7d78b691ce0
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-02 12:51:06 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240402/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int prod=1;
+    cin  >> n;
+    for(int i = 1; i <= 10 ; i++){
+        prod*=i;
+        if(prod>=n)
+            {
+                cout << i;
+                break;
+            }
+    }
+    return 0;
+}
