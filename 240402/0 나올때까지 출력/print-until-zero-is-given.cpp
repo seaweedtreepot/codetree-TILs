145dec03fd95872038fefdc173504b89577923a0
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-02 13:01:08 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240402/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    while(1){
+        cin >> n;
+        if(n!=0){
+            cout << n<<  "\n";
+        }else{
+            break;
+        }
+    }
+    return 0;
+}
