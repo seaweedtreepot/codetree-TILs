6519ffcf5846a1f4862330150260f252953d003e
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-02 13:43:32 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240402/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    bool flag = false;
+
+    for(int i=2; i < n; i++){
+        if(n%i==0){
+            flag == true;
+            break;
+        }
+    }
+
+    cout << (flag?"C":"N");
+    return 0;
+}
