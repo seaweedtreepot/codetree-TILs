7264f119d1ce96cae2091a49266ff3b8a0dbd0ba
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-02 13:03:07 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240402/특정 숫자 맞추기/catching-specific-number.cpp
+++ 240402/특정 숫자 맞추기/catching-specific-number.cpp
@@ -7,7 +7,7 @@ int main() {
     while(1){
         cin >> n;
         if(n<25){
-            cout << "Higer" << endl;
+            cout << "Higher" << endl;
         }else if(n>25){
             cout << "Lower" << endl;
         }
