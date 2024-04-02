99ce3b987e9d26741735a74fda26e1b838bbd8d0
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-02 13:41:05 +0900

    Title: ab 사이에 있는 c, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240402/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, ,c;
+    bool flag = false;
+
+    for(int i = a; i <b +1; i++){
+        if(i%c==0){
+            flag = true;
+        }
+    }
+
+    cout << (flag?:"YES"?"NO");
+    return 0;
+}
