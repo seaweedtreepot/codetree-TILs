8cf9347c360c708fe1f634bfe465cffcc6b78c8a
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-03 15:28:45 +0900

    Title: 별표 출력하기 9, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240403/별표 출력하기 9/print-star-9.cpp
+++ 240403/별표 출력하기 9/print-star-9.cpp
@@ -5,10 +5,10 @@ int main() {
     int n =0; cin>> n;
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < (2*(n-1-i))){
+        for(int j = 0; j < (2*(n-1-i));j++){
             cout << " ";
         }
-        for(int j = 0; j < 2*i+1){
+        for(int j = 0; j < 2*i+1;j++){
             cout << "* ";
         }
 
