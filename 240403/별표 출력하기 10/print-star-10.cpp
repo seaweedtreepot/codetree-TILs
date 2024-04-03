6b03f0f0287d8d6ea5a5e42b85dc643034e44c69
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-03 16:26:05 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240403/별표 출력하기 10/print-star-10.cpp
+++ 240403/별표 출력하기 10/print-star-10.cpp
@@ -3,17 +3,37 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n; cin>> n;
-
+    int cnt_o=1, cnt_e=n;
     for(int i = 0; i < n; i++){
         if(i%2==0){
-
+            for(int j = 0; j < cnt_o; j++){
+                cout << "* ";
+            }
+            cnt_o++;
+            cout << endl;
         }else{
-
+            for(int j = 0; j <cnt_e;j++){
+                cout << "* ";
+            }
+            cnt_e--;
+            cout << endl;
         }
     }
    
     for(int i = 0; i < n; i++){
-        
+        if(i%2==0){
+            for(int j = 0; j < cnt_e; j++){
+                cout << "* ";
+            }
+            cout << endl;
+            cnt_e--;
+        }else{
+            for(int j =0; j<cnt_o;j++){
+                cout << "* ";
+            }
+            cout << endl;
+            cnt_o++;
+        }
     }
     return 0;
 }
