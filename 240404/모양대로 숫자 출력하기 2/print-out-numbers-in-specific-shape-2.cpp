b4b4ead03f0a89434028112e8665e78f23c881bc
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-04 10:11:08 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240404/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
+++ 240404/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
@@ -8,12 +8,13 @@ int main() {
     int cnt =1;
     for(int i = 0 ; i < n; i++){
         for(int j = 0 ; j < n ; j++){
-            cout << cnt*2;
+            cout << cnt*2 <<" ";
             cnt++;
             if(cnt>4){
                 cnt=1;
             } 
         }
+        cout << endl;
     }
     return 0;
 }
