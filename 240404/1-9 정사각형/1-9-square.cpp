37117845d6f80ae4f89d1f1e945a66a7c8441c73
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-04 10:06:15 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240404/1-9 정사각형/1-9-square.cpp
+++ 240404/1-9 정사각형/1-9-square.cpp
@@ -7,8 +7,9 @@ int main() {
     int cnt=0;
     for(int i = 0; i < n ; i++){
         for(int j = 0; j < n; j++){
+            cnt%=9;
             cout << ++cnt;
-            cnt%=10;
+           
         }
         cout << endl;
     }
