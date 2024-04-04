7f53d239b85b369143f34f3dc07dc8de85e4a1ac
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-04 09:55:47 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를  작성해주세요.
+    int n=0;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << n-j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
