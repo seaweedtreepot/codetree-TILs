ef9ae2f5a5d9f00a7dd3351a89c1efd2aa4c6194
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-04 09:42:17 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240404/별표 출력하기 12/print-star-12.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i==0){
+                cout <<"* ";
+            }
+            else if(j>=i && j%2==1){
+                cout << "* ";    
+            }
+            else{
+                cout << "  ";
+            }
+
+        }
+      
+        cout << endl;
+    }
+    return 0;
+}
