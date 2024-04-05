20c33e905c9567e95979b261a4e87c5fe9350ac8
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-05 12:59:57 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240405/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt=0;
+    int a;
+    for(int i = 0;i < 10; i++){
+        cin >>a ;
+        cnt += a;
+    }
+
+    cout << a;
+    return 0;
+}
