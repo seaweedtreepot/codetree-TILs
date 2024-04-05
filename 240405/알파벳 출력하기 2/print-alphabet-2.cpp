e9a05b6e6f888a8e02015d3ba8b88055f6234461
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-05 12:34:03 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    char cnt = 'A';
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(j<i){
+                cout << "  ";
+            }else{
+                cout << cnt++<<" ";
+                if(cnt > 'Z'){
+                    cnt = 'A';
+                }
+            }
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
