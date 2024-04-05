4db691fdcea959e7ff96d92d5b32a81b85890c70
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-05 12:10:12 +0900

    Title: 모양대로 숫자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i<=j){
+                cout << n-j <<" ";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
