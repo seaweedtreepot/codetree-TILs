b1afca44554761141c83f31d2c5053881de8dec5
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-05 12:08:00 +0900

    Title: 숫자 피라미드 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를  작성해주세요.
+    int cnt=1;
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout << cnt++ << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
