5ee784ad9e70ff68abf02a8709f5f9bee486cfdb
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-05 12:53:08 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240405/소수 구하기/get-prime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for(int i = 2; i <= n; i++){
+        bool flag=false;
+        for(int j = 2; j <i; j++){
+            if(i%j==0){
+                flag = true;
+                break;
+            }
+        }
+        if(flag==false){
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
