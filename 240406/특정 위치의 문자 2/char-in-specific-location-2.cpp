db50bd57bc2c75213651923b0ef9f4ab2d3ecfa7
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-06 10:44:03 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/특정 위치의 문자 2/char-in-specific-location-2.cpp
+++ 240406/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int arr[10] = {};
+    char arr[10] = {};
 
     for(int i = 0; i < 10; i++){
         cin >> arr[i];
     }
-    cout << (char)arr[1] << " " << (char)arr[4] << " " << (char)arr[7];
+    cout << arr[1] << " " << arr[4] << " " << arr[7];
     return 0;
 }
