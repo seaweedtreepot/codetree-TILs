b133a438f897f50b6c10506eefe18ae721b1a85f
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-06 11:01:36 +0900

    Title: 배열에서 원소의 특징, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240406/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
+++ 240406/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -7,7 +7,8 @@ int main() {
     for(int i = 0; i < 10; i++){
         cin >> arr[i];
         if(arr[i]%3==0){
-            cout << arr[i-1];break;
+            cout << arr[i-1];
+            break;
         }
     }
     return 0;
