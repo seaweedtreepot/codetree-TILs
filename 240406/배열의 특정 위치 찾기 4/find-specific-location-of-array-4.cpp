37eca8ff46db3cc670043aa20795cde4c38391ac
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-06 10:01:13 +0900

    Title: 배열의 특정 위치 찾기 4, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240406/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int cnt=0;
+    int sum=0;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(arr[i]==0){
+            break;
+        }
+        if(arr[i]%2==0){
+            cnt++;
+            sum += arr[i];
+        }
+    }
+
+    cout << cnt << " " << sum;
+    return 0;
+}
