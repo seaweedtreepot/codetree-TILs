4ab1deccb0c5f9c1795e14ab1f9283ddfcc2e5f6
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-06 11:04:55 +0900

    Title: 제곱하여 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240406/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        cout << arr[i]* arr[i] << " ";
+    }
+    return 0;
+}
