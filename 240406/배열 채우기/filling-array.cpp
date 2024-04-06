d4fdfa442de4a4bbc06caef981e9911b196955d1
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-06 09:53:22 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/배열 채우기/filling-array.cpp
+++ 240406/배열 채우기/filling-array.cpp
@@ -3,13 +3,12 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int arr[10];
-    int cnt=0;
+    int cnt=-1;
 
 
     for(int i = 0; i < 10; i++){
         cin >> arr[i];
         if(arr[i]==0){
-            cnt--;
             break;
         }
         cnt++;
