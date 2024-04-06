908be88e71a220793143beb845ac374d096399d5
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-06 11:11:45 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/일의 자리 배열/array-with-units.cpp
+++ 240406/일의 자리 배열/array-with-units.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a ,b;
-    cin >> a >> b;
-    cout <<a << " " << b << " ";
+    int arr[10];
+    cin >> arr[0] >> arr[1];
+    cout << arr[0] <<" " << arr[1] << " ";
+
     for(int i = 2; i < 10; i++){
-        int temp_1 = a + b;
-        a = b;
-        b = temp_1;
-        cout << temp_1%10 << " ";
+        arr[i] = arr[i-2] + arr[i-1];
+        cout << arr[i]%10 << " ";
     }
+   
     return 0;
 }
