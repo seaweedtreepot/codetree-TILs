1b6b1fee78764d57073f124d488cd37261207453
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-07 10:39:51 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240407/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[10]={};
+
+    for(int i = 0; i < n; i++){
+        cin >> n;
+        arr[i]++;
+    }
+
+    for(int i = 1; i < 10; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
