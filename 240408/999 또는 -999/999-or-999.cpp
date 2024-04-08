a62c4d34dac76b827ae1cf7a52b8eb75422dcd7f
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-08 11:57:23 +0900

    Title: 999 또는 -999, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240408/999 또는 -999/999-or-999.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[100] = {};
+    int cnt = 0;
+    int min_val = INT_MAX, max_val = INT_MIN;
+    while(1){
+        cin >> arr[cnt++];
+        if(arr[cnt-1]==999 || arr[cnt-1]==(-999)){
+            break;
+        }
+        if(arr[cnt-1]>max_val){
+            max_val = arr[cnt-1];
+        }else if(arr[cnt-1]<min_val){
+            min_val = arr[cnt-1];
+        }
+    }
+
+
+    cout << max_val << " " << min_val ;
+    return 0;
+}
