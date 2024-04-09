92e0de46be90189d0ce096287e5ec4a9f6a25afc
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-09 13:48:12 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240409/더 긴 문자열/longer-string.cpp
+++ 240409/더 긴 문자열/longer-string.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    string str1; str2;
+    string str1, str2;
     cin >> str1 >> str2;
 
     if(str1.length()>str2.length()){
-        cout << str1 ;
+        cout << str1  << " " << str1.length();
     }else if (str1.length()==str2.length()){
         cout << "same";
     }else{
-        cout << str2;
+        cout << str2 << " " << str2.length();
     }
     return 0;
 }
