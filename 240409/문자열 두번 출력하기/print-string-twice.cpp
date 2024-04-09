5856055afd9e713fea4546dc655d858ae30df7f1
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-09 13:37:46 +0900

    Title: 문자열 두번 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240409/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    cout << str << "\n" << str;
+    return 0;
+}
