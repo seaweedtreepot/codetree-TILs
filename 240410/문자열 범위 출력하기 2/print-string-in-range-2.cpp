7e33a37d23a0ec36238e3a051449119db6f92de3
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-10 12:56:18 +0900

    Title: 문자열 범위 출력하기 2, Time: 19ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240410/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240410/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     string str;
-    int n; cin >> str;
+    int n; 
+    cin >> str;
     cin >> n;
+
+
     if(str.length()<n){
         for(int i = str.length()-1; i >=0; i--){
-        cout << str[i];
-    } 
+            cout << str[i];
+        } 
     }else{
-      for(int i = str.length()-1; i >=str.length()-n; i--){
-        cout << str[i];
-    }
+        for(int i = str.length()-1; i >=str.length()-n; i--){
+            cout << str[i];
+        }
     }
     
     return 0;
