a4f824b9203e5096494ec407e8c1e2301f83478c
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-12 12:12:33 +0900

    Title: 문자열 돌리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240412/문자열 돌리기/SPin-SPring.cpp
+++ 240412/문자열 돌리기/SPin-SPring.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     string a;
     cin >> a;
-
+    cout << a << endl;
     for(int i = 0; i < a.length();i++){
         a = a[a.length()-1] + a.substr(0,a.length()-1);
         cout << a << endl;
