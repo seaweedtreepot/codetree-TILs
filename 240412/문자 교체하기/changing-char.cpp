ad25ee7f191c6f114a696798522254926b7820e3
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-12 11:08:56 +0900

    Title: 문자 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/문자 교체하기/changing-char.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a, b;
+    cin >> a >> b;
+
+    b[0] = a[0];
+    b[1] = a[1];
+    cout << b;
+    return 0;
+}
