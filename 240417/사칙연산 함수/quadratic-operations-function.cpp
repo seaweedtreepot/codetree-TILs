95e45bf1e5ebd644e298866292bc3dcb6cf59353
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-17 06:24:12 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240417/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void Plus(int a, int c){
+cout << a << " + " << c << " = " << a+c << endl;
+}
+
+void Minus(int a, int c){
+    cout << a << " - " << c << " = " << a/c << endl;
+}
+
+void Multiply(int a, int c){
+    cout << a << " * " << c << " = " << a/c << endl;
+}
+
+void Divide(int a, int c){
+    cout << a << " / " << c << " = " << a/c << endl; 
+}
+
+void Calculate(int a, int c, char o){
+    if(o == '+'){
+        Plus(a,c);
+    }else if(o=='-'){
+        Minus(a,c);
+    }else if(o=='/'){
+        Divide(a,c);
+    }else if(o=='*'){
+        Multiply(a,c);
+    }else {
+        cout << "False";
+    }
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, c;
+    char o;
+    cin >> a >> o >> c;
+    Calculate(a,c,o);
+    return 0;
+}
