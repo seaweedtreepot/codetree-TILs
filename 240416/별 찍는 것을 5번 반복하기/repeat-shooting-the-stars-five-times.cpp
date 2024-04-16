2d87dcad6eaa1681e58c7740bb36e934092fdcf9
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-16 20:21:19 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240416/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void Print10Stars(){
+    cout <<"********** ";
+}
+int main() {
+    // 여기에 코드를 작성 해주세요.
+    for(int i = 0; i < 5; i++){
+        Print10Stars();
+    }
+    return 0;
+}
