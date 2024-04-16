9f49242026fadf0685580c0e02c53b536c0a5baf
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-16 21:26:14 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240416/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
+++ 240416/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
@@ -4,11 +4,12 @@ using namespace std;
 bool IsMagic_num(int n){
     bool flag = false;
     if((n/10 + n%10)%5==0){
+        if(n%2==0){
         flag = true;
     }
-    if(n%2==0){
-        flag = true;
+
     }
+    
 
 
     return flag;
