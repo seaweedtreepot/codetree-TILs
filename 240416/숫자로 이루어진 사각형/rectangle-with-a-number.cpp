f85c016b980d8d7b9e0371a17170abe7e45cedc5
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-16 20:44:53 +0900

    Title: 숫자로 이루어진 사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240416/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void PrintStars(int n){
+    int cnt = 1;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << cnt++ << " ";
+            if(cnt>9){
+                cnt = 1;
+            } 
+        }
+        cout << endl;
+    }
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    PrintStars(n);
+    return 0;
+}
