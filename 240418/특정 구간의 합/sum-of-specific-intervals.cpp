1f1b48c32c6681dc64e07873856dd83609d29bd1
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-18 14:23:17 +0900

    Title: 특정 구간의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240418/특정 구간의 합/sum-of-specific-intervals.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int arr[100];
+void SumOfPart(int m){
+    int a1, a2;
+    for(int i = 0; i < m ; i++){
+        cin >> a1 >> a2;
+        int sum = 0;
+        for(int j = a1-1; j <a2; j++){
+            sum += arr[j];
+        }
+        cout << sum<< endl;
+    }
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    SumOfPart(m);
+    return 0;
+}
