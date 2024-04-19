8865d801670ee14b403f6db1f40cd9f335bf0c1e
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-19 11:53:45 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void PrintStars(int N){
+    if(N==0){
+        return;
+    }
+    for(int i = 0; i < N; i++){
+        cout << "*"<<" ";
+    }
+    cout << endl;
+    PrintStars(N-1);
+    for(int i = 0; i < N; i++){
+        cout << "*"<<" ";
+    }
+    cout << endl;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N;
+    cin >> N;
+    PrintStars(N);
+    return 0;
+}
