6956ec6b06cf3561a65f456f0096aed332ec4e44
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-22 12:39:57 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 295ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 240422/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 240422/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -2,48 +2,27 @@
 using namespace std;
 
 int Smallest(int arr[],int idx){
-    if(idx == 0){
-        int small = 1;
-        int num1 = arr[idx];
-        int num2 = arr[idx+1];
-        if(num1 < num2){
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
+        int small = 0;
+        int num1;
+        if(idx == 0){
+            num1 = arr[idx];
+        }else{
+            num1 = Smallest(arr,idx-1);
         }
-
-        while(1){
-            if(num1%num2==0){
-                //cout << "Excape" << endl;
-               // cout << num1 << " " << num2 << endl;
-                small = num2;
-                break;
-            }
-        num1 = num1%num2;
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
-        }
-
-        small = (arr[idx]/small) * (arr[idx+1]/small) * small;
-        return small;
-    }
-        int small = 1;
-        int num1 = Smallest(arr,idx-1);
+        
         int num1_1 = num1;
         int num2 = arr[idx+1];
+
         if(num1 < num2){
             int temp = num1;
            num1 = num2;
             num2 = temp;
         }
-     while(1){
+        while(1){
             if(num1%num2==0){
-                //cout << "Escape: " << idx << endl; 
-               // cout << num1 << " " << num2 << endl;
                 small = num2;
                 break;
-        }
+            }
         num1 = num1%num2;
         int temp = num1;
         num1 = num2;
@@ -57,7 +36,6 @@ int Smallest(int arr[],int idx){
 
 int main() {
     // 여기에 코드를 작성해주세요.
-
     int n;
     int arr[10];
     cin >> n;
