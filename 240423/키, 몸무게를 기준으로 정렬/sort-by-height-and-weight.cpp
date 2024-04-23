c2ba8fc85d5940f88a0eb9288296b30c967e16a2
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-23 12:34:23 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240423/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Person{
+    public:
+    string name;
+    int height;
+    int weight;
+    Person(){};
+};
+
+bool cmp(Person a, Person b){
+    if(a.height == b.height){
+        return a.weight > b.weight;
+    }
+    return a.height > b.height;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; cin >> n;
+    Person arr[10];
+    string name; int a ,b ;
+    for(int i =0; i < n; i++){
+        cin >> name >> a >> b;
+        arr[i].name = name;
+        arr[i].height = a;
+        arr[i].weight = b;
+    }
+
+    sort(arr,arr+n,cmp);
+
+    for(int i =0; i < n; i++){
+        cout << arr[i].name << " " << arr[i].height 
+        << " "<< arr[i].weight << endl;
+    }
+
+    return 0;
+}
