35e61e882b42c44e1bfa66289f362843313baad9
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-23 12:16:22 +0900

    Title: 국영수 순이지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240423/국영수 순이지/korean-english-math-order.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Students{
+    public:
+    string name;
+    int kor;
+    int ma;
+    int eng;
+
+    Students(){};
+};
+
+bool cmp(Students a, Students b){
+    if(a.kor == b.kor){
+        if(a.eng == b.eng){
+            return a.ma > b.ma;
+        }
+        return a.eng > b.eng;
+    }
+    return a.kor > b.kor;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    Students st[10];
+    int n;
+    cin >> n;
+    string z;
+    int a,b,c;
+    for(int i = 0; i< n; i++){
+        cin >> z>> a >> b >> c;
+        st[i].kor =a;
+        st[i].eng =b;
+        st[i].ma =c;
+        st[i].name=z;
+    }
+
+    sort(st,st+n,cmp);
+    for(int i =0; i < n; i++){
+        cout << st[i].name 
+        << " "<< st[i].kor << " " << st[i].eng << " "<<st[i].ma << endl;
+    }
+    return 0;
+}
