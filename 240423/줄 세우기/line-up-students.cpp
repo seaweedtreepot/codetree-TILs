3a6376b286e5279e5e15c33b1d7ef94ccbc47fa9
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-23 12:23:33 +0900

    Title: 줄 세우기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240423/줄 세우기/line-up-students.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Students{
+    public:
+    int weight;
+    int height;
+    int number;
+
+    Students(){};
+};
+
+bool cmp(Students a, Students b){
+    if(a.height == b.height){
+        if(a.weight == b.weight){
+            return a.number < b.number;
+        }
+        return a.weight > b.weight;
+    }
+    return a.height > b.height;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    Students st[10];
+    int n;
+    cin >> n;
+    int a,b;
+    for(int i = 0; i< n; i++){
+        cin >> a >> b;
+        st[i].weight =b;
+        st[i].height =a;
+        st[i].number =i+1;
+    }
+
+    sort(st,st+n,cmp);
+    for(int i =0; i < n; i++){
+        cout << st[i].height
+        << " "<< st[i].weight << " " << st[i].number <<endl;
+    }
+    return 0;
+}
