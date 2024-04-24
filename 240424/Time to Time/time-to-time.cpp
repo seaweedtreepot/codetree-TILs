b4da4ba8c08b7a72fd9099599d41fe7b2128ad1d
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-24 12:44:26 +0900

    Title: Time to Time, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240424/Time to Time/time-to-time.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    a *= 60;
+    c *= 60;
+    a+= b;
+    c+= d;
+    cout << c - a;
+    return 0;
+}
