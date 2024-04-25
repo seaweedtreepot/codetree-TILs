fff6478b64ca4e632f208d62a6d99ffd763995f4
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-25 16:17:02 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240425/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240425/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -7,26 +7,33 @@ int main() {
     int current=0;
     char cur;
     int arr[2001]={};
+    arr[1000]= 1;
+
     for(int i =0; i < n; i++){
         cin >> x >> cur;
+        arr[current+1000]++;
         if(cur == 'R'){
             while(x!=0){
                 current++;
                 arr[current+1000]++;
                 x--;
             }
-        }else{
+            
+        }else if (cur=='L'){
             while(x!=0){
                 current--;
                 arr[current+1000]++;
                 x--;
             }
         }
+        //cout << current << endl;
     }
+    cout << endl;
     int cnt =0;
-    for(int i = 0; i < 2001;i++){
-        if(arr[i]>=2 && arr[i]>=2){
+    for(int i = 0; i < 2000;i++){
+        if(arr[i]>=2 && arr[i+1]>=2){
             cnt++;
+         //   cout << i-1000 << " " << i-999 << endl;
         }
     }
     cout << cnt;
