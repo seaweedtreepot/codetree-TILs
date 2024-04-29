9713b1f74cbacb869414a3a39cf588974d7375d4
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-04-29 12:45:45 +0900

    Title: 만나는 그 순간, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240429/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_arr[1000]={};
+    int b_arr[1000]={};
+    int n, m,cnt=1,locate = 0;   
+    cin >> n >> m;
+    int cur;
+    char dir,prev;
+    for(int i = 0; i < n; i++){
+        cin >> dir >> cur;
+        
+        for(int j =0; j < cur; j++){
+            if(dir=='R'){
+                a_arr[cnt++]=++locate;
+            }else{
+                a_arr[cnt++]=--locate;
+            }
+        }
+    }
+
+    for(int i = 0; i < cnt; i++){
+        //cout << a_arr[i] << endl;
+    }
+    cnt = 1;
+    locate = 0;
+    for(int i = 0; i < m; i++){
+        cin >> dir >> cur;
+        for(int j =0; j < cur; j++){
+            if(dir=='R'){
+                b_arr[cnt++]=++locate;
+            }else{
+                b_arr[cnt++]=--locate;
+            }
+        }
+    }
+    for(int i = 0; i < cnt; i++){
+        //cout << b_arr[i] << endl;
+    }
+    for(int i = 1; i < cnt; i++){
+        //cout << i <<" : " << a_arr[i] << " " << b_arr[i] << endl;
+        if(a_arr[i]==b_arr[i]){
+            cout << i;
+            return 0;
+        }
+    }
+
+    cout << -1;
+    return 0;
+}
