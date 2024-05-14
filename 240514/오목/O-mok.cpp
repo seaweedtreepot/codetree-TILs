ad05522ea0492cddd4bb41095574946d8fc0e9d4
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-05-14 11:57:22 +0900

    Title: 오목, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240514/오목/O-mok.cpp
+++ 240514/오목/O-mok.cpp
@@ -57,8 +57,8 @@ int main() {
     int n= 19;
     int ans_col=0,ans_row=0;
     int flag = false;
-    for(int i = 0; i < n-4; i++){
-        for(int j = 0; j < n-4; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             if(arr[i][j]==arr[i][j+1]&& arr[i][j]!=0){
                 if(IsWin1(arr,i,j)){
                     cnt = arr[i][j];
