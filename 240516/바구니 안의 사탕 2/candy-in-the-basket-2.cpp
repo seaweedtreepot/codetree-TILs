612a29bbf04d6ed6dccafc2f8cb3eb76f8f0af00
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-05-16 13:32:20 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240516/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,k;
+    cin >> n >> k;
+    int arr[101]={};
+    int cur,cur2;
+    for(int i = 0; i < n; i++){
+        cin >> cur >> cur2;
+        arr[cur2] += cur;
+    }
+    int max = 0;
+    for(int i = k; i < 101-k ;i++){
+        int sum = 0;
+        for(int j = i-k; j <= i+k; j++){
+            sum += arr[j];
+        }
+        if(max < sum){
+            //cout << i << " " << sum << endl;
+            max = sum;
+        }
+    }
+
+    cout << max;
+    return 0;
+}
