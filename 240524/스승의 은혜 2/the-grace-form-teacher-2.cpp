8d4a9e10ec8bd6c354e47969a7d569a9c9e7acae
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-05-24 15:23:05 +0900

    Title: 스승의 은혜 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240524/스승의 은혜 2/the-grace-form-teacher-2.cpp
+++ 240524/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <utility>
+#include <algorithm>
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -12,31 +13,24 @@ int main() {
     for(int i = 0;i < n; i++){//p(i)삽입
         cin >> stdt[i];
     }
-
+    sort(stdt,stdt+n);
     for(int i = 0; i < n; i++){//할인
         int cnt = 0;
         int price = 0;
-        for(int j = i; j < n; j++){//시작
-            for(int k = j; k < n; k++){//더하기
-                if(k==i){
-                    if(price + stdt[k]/2>b){
-                        continue;
-                    }else{
-                        price += stdt[k]/2;
-                        cnt++;
-                    }
-                }else{
-                    if(price + stdt[k]>b){
-                        continue;
-                    }else{
-                        price += stdt[k];
-                        cnt++;
-                    }
-                }
-                if(max < cnt ){
-                    max = cnt;
-                }
+        for(int j = 0; j < n; j++){//시작
+            if(i==j){
+                price+=stdt[j]/2;
+            }else{
+                price += stdt[j];
             }
+            if(price > b){
+                break;
+            }else{
+                cnt++;
+            }
+        }
+        if(cnt > max){
+            max = cnt;
         }
     }
 
