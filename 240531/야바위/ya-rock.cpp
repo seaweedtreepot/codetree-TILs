242db0407f97ce9473ddb27f94e930eb29b56c9c
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-05-31 10:54:12 +0900

    Title: 야바위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240531/야바위/ya-rock.cpp
+++ 240531/야바위/ya-rock.cpp
@@ -2,27 +2,54 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,a,b,c,score = 0,cnt=0,max=0;
+    int n,a,b,c,max=0;
     cin >> n;
 
-    for(int i = 1; i < 4; i++){//i번컵일대 최대점수
-        bool arr[4]= {};
-        arr[i]= true;
-        score =0;
+    //i번컵일대 최대점수
+        bool arr1[4]= {};
+        bool arr2[4] = {};
+        bool arr3[4] = {};
+        arr1[1]= true;
+        arr2[2]=true;
+        arr3[3] = true;
+        int score1 = 0,score2=0,score3=0;
         for(int j = 0; j < n; j++){
             cin >> a >> b >> c;
-            bool pram = arr[a];
-            arr[a] = arr[b];
-            arr[b] = pram;
 
-            if(arr[c]==true){
-                score++;
+            bool pram = arr1[a];
+            arr1[a] = arr1[b];
+            arr1[b] = pram;
+
+            pram = arr2[a];
+            arr2[a] = arr2[b];
+            arr2[b] = pram;
+
+            pram = arr3[a];
+            arr3[a] = arr3[b];
+            arr3[b] = pram;
+
+            
+            if(arr1[c]){
+                score1++;
+            }
+            if(arr2[c]){
+                score2++;
+            }
+            if(arr3[c]){
+                score3++;
             }
         }
-        if(score > max){
-            max = score;
+        //cout <<"::"<< score <<endl;
+        if(score1 > max){
+            max = score1;
         }
-    }
+        if(score2>max){
+            max = score2;
+        }
+        if(score3>max){
+            max = score3;
+        }
+    
 
     cout << max;
     return 0;
