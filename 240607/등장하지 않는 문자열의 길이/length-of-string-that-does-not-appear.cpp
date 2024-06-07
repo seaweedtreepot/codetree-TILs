71a608a8583ea0e36ee2bf9d82997cfbd3de42fd
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-07 11:50:37 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
+++ 240607/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -5,40 +5,37 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     string str;
-    string dupl = "";
-    bool flag = false;
+
     cin >> n >> str;
 
-    for(int i = 1; i < n/2+1; i++){//길이
-        dupl = "";
-        flag = false;//
-        for(int q = 0; q < n-i+1; q++){//start point
-            
-            dupl = str.substr(q,i);
-            string ace = str; 
-            ace.erase(q,i);
-            //cout << ace << " "<< dupl << endl;
-            if(ace.find(dupl)!=string::npos){
-                flag = true;
+    for(int i = 1; i < n; i++){
+        bool flag = false;
+        int ans = i;
+        for(int q = 0; q < n-i+1;q++){
+            string duple = str.substr(q,i);
+            //cout << duple << endl;
+            int cnt = 0;
+            for(int k = 0; k < n-i+1;k++){
+                if(duple == str.substr(k,i)){
+                    //cout << duple << " " ;
+                    cnt++;
+                }
+                if(cnt > 1){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag){
                 break;
+            }else{
+                ans = duple.length();
             }
         }
         if(!flag){
-            //cout << 1 <<endl;
-            break;
-        }else{
-            if(i==n/2){
-                //cout << 2 << endl;
-                flag = false;
-            }
+            cout << ans << endl;
+            return 0;
         }
+        
     }
-
-    if(!flag && n%2==1){
-        cout << dupl.length()+1;
-    }else{
-        cout << dupl.length();
-    }
-    
     return 0;
 }
