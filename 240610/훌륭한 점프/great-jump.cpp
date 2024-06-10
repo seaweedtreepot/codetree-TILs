379c8673bcfd179836f88160d1662cfcde9b99c3
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-10 12:15:36 +0900

    Title: 훌륭한 점프, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240610/훌륭한 점프/great-jump.cpp
+++ 240610/훌륭한 점프/great-jump.cpp
@@ -2,40 +2,31 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,k,ans_min=0;
+    int n,k,arr[100];
     cin >> n >> k;
-    int arr[100];
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++){
         cin >> arr[i];
-    int cur = 0;
-    ans_min = arr[0];
-
-
-    for(int i = 0; i < n; i++){//
-        int min = 100;
-        i = cur;
-        for(int j = 1; j <= k; j++){//착지지점 정하기
-            if(i+1 == n-1){//다음에 뛰는곳이 무조건 마지막 지점일 경우
-                if(arr[n-1]>ans_min){
-                    cout << arr[n-1];
-                    return 0;
-                }else{
-                    cout << ans_min;
-                    return 0;
-                }
-            }
-            if(min > arr[i+j]){
-                min = arr[i+j];
-                cur = i + j;
-                
-            }
-        }
-        if(arr[cur]>ans_min){
-            //cout << cur << " " << arr[cur]<< endl;
-            ans_min = arr[cur];
-        }
     }
-
-    cout << ans_min;
+    int ans = arr[0];
+    int cur = 0;
+    while(cur < n){
+        int min = 101;
+        int jump;
+        for(int j = cur+1; j <= k+cur;j++){
+            if(j == n-1){
+                cout << (ans>arr[n-1]? ans : arr[n-1]);
+                return 0;
+            }
+            if(arr[j]<min){
+                jump = j;
+                min = arr[j];
+            }
+        }
+        if(arr[jump] > ans){
+            ans = arr[jump];
+        }
+        cur = jump;
+        //cout << cur << endl;
+    }
     return 0;
 }
