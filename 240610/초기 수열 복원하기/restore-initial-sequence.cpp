4378c94cb1024baa872301a03f5ff7cff5bec137
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-10 14:19:18 +0900

    Title: 초기 수열 복원하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240610/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int arr[1000];
+    cin >> n;
+    for(int i = 0; i < n-1; i++){
+        cin >> arr[i];
+    }
+
+    int ans_arr[1000];
+
+    for(int j = 1; j < 10; j++){
+        bool checking[1001] = {};
+        bool flag = true;
+        for(int i = 0; i < n; i++){
+            if(i==0){
+                ans_arr[i] = j;
+                checking[j]=true;
+            }else{
+               ans_arr[i] = arr[i-1] - ans_arr[i-1];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            checking[ans_arr[i]] = true;
+            //cout << ans_arr[i] << " ";
+        }
+        //cout << endl;
+        for(int i = 1; i <= n; i++){
+            checking[ans_arr[i]] = true;
+        }
+        for(int i = 1; i<=n;i++){
+            if(checking[i]!=true){
+                flag =false;
+            }
+        }
+
+        if(flag){
+            for(int q = 0; q < n; q++){
+                cout << ans_arr[q] << " ";
+            }
+            return 0;
+        }
+        
+    }
+
+    
+    return 0;
+}
