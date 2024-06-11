ae054fbc805c097cbe4ca8fb5a727f47f25935f7
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-11 16:17:10 +0900

    Title: 구간 잘 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240611/구간 잘 나누기/divide-sections-well.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m,arr[100];
+    cin >> n >> m ;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int ans = INT_MAX;
+
+
+    for(int i = 1; i < 100; i++){//구간당 최대값
+        int max=  0;
+        int mcnt = 0;
+        for(int j = 0; j < n; j++){
+            max += arr[j];
+            if(max > i){
+                max = arr[j];
+                mcnt++;
+                //cout << i << " : " << max << " : " << mcnt<<" : "<< j << endl;
+                
+            }
+            if(mcnt>m-1){
+                break;
+            }
+        }
+        if(mcnt == m-1){
+            cout << i<<endl;
+            return 0;
+        }
+
+    }
+    return 0;
+}
