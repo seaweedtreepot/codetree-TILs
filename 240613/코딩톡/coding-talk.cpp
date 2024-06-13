70040f1cfc0d79279732c0d8b21089c15c1fc6a0
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-13 20:17:57 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240613/코딩톡/coding-talk.cpp
+++ 240613/코딩톡/coding-talk.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
+#include <utility>
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n,m,p;
     bool alphabet[26] ={};
+    pair<int,int> log[100];
+
     cin >> n >> m >> p;
-    
     p--;
+    
     char t1;
     int t2;
+    
     for(int i = 0; i < m; i++){
         cin >> t1 >> t2;
-        if(i==p && t2==0){
-            return 0;
-        }
-        if(i >= p){
-            
-            alphabet[t1-'A']=true;;
-        }
-    }    
+        log[i] = make_pair(t1-'A',t2);
+    }
 
+   
+    if(log[p].second == 0){
+        cout << "stop";
+        return 0;
+    }
+
+    for(int i = p; i < n; i++){
+        alphabet[log[i].first] = true;
+    }
+    for(int i = p ; i >= 0; i--){
+        if(log[p].second == log[i].second){
+            alphabet[log[i].first] = true;
+        }else{
+            break;
+        }
+    }
     for(int i = 0; i < n; i++){
         if(!alphabet[i]){
-            cout << static_cast<char>(i+'A') << " ";
+            cout << (char)(i+'A') << " ";
         }
     }
     return 0;
