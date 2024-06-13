d945993736d955fd95902e342880be198bdf3afc
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-13 19:35:57 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240613/코딩톡/coding-talk.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m,p;
+    bool alphabet[26] ={};
+    cin >> n >> m >> p;
+    p--;
+    char t1;
+    int t2;
+    for(int i = 0; i < m; i++){
+        cin >> t1 >> t2;
+        if(i >= p){
+            alphabet[t1-'A']=true;;
+        }
+    }    
+
+    for(int i = 0; i < n; i++){
+        if(!alphabet[i]){
+            cout << static_cast<char>(i+'A') << " ";
+        }
+    }
+    return 0;
+}
