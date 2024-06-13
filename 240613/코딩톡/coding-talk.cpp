82f78d4e3491b3f95e529f24d0ba53271c6899e0
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-13 19:38:01 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240613/코딩톡/coding-talk.cpp
+++ 240613/코딩톡/coding-talk.cpp
@@ -5,12 +5,17 @@ int main() {
     int n,m,p;
     bool alphabet[26] ={};
     cin >> n >> m >> p;
+    
     p--;
     char t1;
     int t2;
     for(int i = 0; i < m; i++){
         cin >> t1 >> t2;
+        if(i==p && t2==0){
+            return 0;
+        }
         if(i >= p){
+            
             alphabet[t1-'A']=true;;
         }
     }    
