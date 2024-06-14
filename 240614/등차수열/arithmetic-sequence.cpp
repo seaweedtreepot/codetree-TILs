207ea4cd1bc689bb830cdc24f737d5d32057b47f
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-14 19:43:47 +0900

    Title: 등차수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240614/등차수열/arithmetic-sequence.cpp
+++ 240614/등차수열/arithmetic-sequence.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <algorithm>
+//#include <algorithm>
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -7,20 +7,13 @@ int main() {
     cin >> n;
     int a;
     int arr[101]={};
-    bool ar[101] = {};
     for(int i = 0; i < n; i++){
        cin >> arr[i];
-       ar[arr[i]] = true;
        
     }
-    sort(arr,arr+n);
-
-    for(int i = 0; i < n; i++){
-        //cout << arr[i] << " ";
-    }
-    //cout << endl;
+    //sort(arr,arr+n);
     int max= 0;
-    for(int k = 01; k <= 100; k++){
+    for(int k = 1; k <= 100; k++){
         int cnt= 0;
         for(int i = 0; i<n; i++){
             for(int j = i+1; j < n; j++){
@@ -32,7 +25,6 @@ int main() {
         }
         if(max< cnt){
             max = cnt;
-            //cout <<k << ": "<< cnt << endl;
         }
     }
 
