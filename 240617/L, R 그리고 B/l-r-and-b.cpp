371b4ed3b21e171ca3d620b11816e49ca62a5412
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-17 14:57:11 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240617/L, R 그리고 B/l-r-and-b.cpp
+++ 240617/L, R 그리고 B/l-r-and-b.cpp
@@ -13,6 +13,8 @@ int main() {
     int Bjidx = 0;
     int Liidx = 0;
     int Ljidx = 0;
+    int Riidx = 0;
+    int Rjidx = 0;
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             if(arr[i][j]=='B'){
@@ -21,11 +23,19 @@ int main() {
             }else if(arr[i][j]=='L'){
                 Liidx = i;
                 Ljidx = j;
+            }else if(arr[i][j]=='R'){
+                Riidx = i;
+                Rjidx = j;
             }
         }
     }
-
-    int ans = abs(Liidx - Biidx) + abs(Ljidx - Bjidx) -1;
+    int cur = -1;
+    if(Riidx == Liidx && Riidx == Biidx ){
+        cur +=2;
+    }else if(Rjidx == Ljidx && Rjidx == Bjidx){
+        cur += 2;
+    }
+    int ans = abs(Liidx - Biidx) + abs(Ljidx - Bjidx) +cur;
     cout << ans;
     return 0;
 }
