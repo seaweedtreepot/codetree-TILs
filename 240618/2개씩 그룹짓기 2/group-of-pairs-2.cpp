6a22ea18cd2f5b97b2368cb69c874a3cbbae28f1
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-18 04:31:43 +0900

    Title: 2개씩 그룹짓기 2, Time: 54ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240618/2개씩 그룹짓기 2/group-of-pairs-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int arr[100000];
+    for(int i = 0; i < 2*n; i++){
+        cin >> arr[i];
+    }
+    sort(arr,arr+2*n);
+    int min = 1000000000;
+    for(int i = 0; i < n; i++){
+        if(arr[n+i]-arr[i] < min){
+            min = abs(arr[n+i]-arr[i]);
+        }
+        //cout << arr[n+i] << " " << arr[i] <<endl;
+    }
+
+    cout << min;
+    return 0;
+}
