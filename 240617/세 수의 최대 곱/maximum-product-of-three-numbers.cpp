5399c2455cdb56f3f68962c477b0d649141a6972
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-17 15:31:05 +0900

    Title: 세 수의 최대 곱, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240617/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[100000];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr,arr+n);
+
+    
+    cout<<endl;
+    int m = arr[0] * arr[1] * arr[n-1];
+    int p = arr[n-1] * arr[n-2] * arr[n-3];
+    int MAX = max(m,p);
+
+    cout << MAX;
+    return 0;
+}
