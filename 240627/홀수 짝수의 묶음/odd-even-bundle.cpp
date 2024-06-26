48e5c11ab30fde40bea658b44dbef43313eb35cf
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-06-27 00:06:12 +0900

    Title: 홀수 짝수의 묶음, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240627/홀수 짝수의 묶음/odd-even-bundle.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int odd,even=0;
+
+    int arr[100];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(arr[i]%2==0){
+            even++;
+        }
+    }
+    odd = n-even;
+    int i = 0;
+    while(1){
+        if(i%2==0){
+            //cout << "짝"<< " ";
+        }else{
+            //cout << "홀" << " ";
+        }
+        //cout << odd << " " << even << endl;
+
+        if(i%2==0){
+            if(even!=0){
+                even--;
+            }else if(odd!=0){
+
+                if(odd==1){
+                    //cout << "발동" << endl;
+                    //cout << i << endl;
+                    i-=2;
+                    //cout << i << endl;
+                    break;
+                }else{
+                    odd-=2;
+                }
+            }
+        }else if(i%2==1){
+            if(odd!=0){
+                odd--;
+            }else{
+                //cout << "q" <<endl;
+                i--;
+                break;
+            }
+        }
+        if(odd==0 && even==0)
+            break; 
+        i++;  
+        //cout << i << endl;
+    }
+
+    cout << i+1;
+    return 0;
+}
