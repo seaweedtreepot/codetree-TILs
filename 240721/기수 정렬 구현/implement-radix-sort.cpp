ea5d145d3d6d7bdd51e375d701a695e9ddda426c
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-07-21 13:07:37 +0900

    Title: 기수 정렬 구현, Time: 51ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240721/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int fun(int n,int a){
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ans = a%10;
+        a/=10;
+    }
+    return ans;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[100000];
+    int bigg =0 ;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(arr[i]>bigg){
+            bigg = arr[i];
+        }
+    }
+    int k = 0;
+    while(bigg!=0){
+        bigg/=10;
+        k++;
+    }
+
+    for(int i = 1; i <= k; i++){
+        vector<vector<int>> v(10);
+        for(int j = 0; j < n; j++){
+            v[fun(i,arr[j])].push_back(arr[j]);
+        }
+        int w = 0;
+        for(int j= 0; j < 10; j++){
+            for(int q = 0; q < v[j].size(); q++){
+                arr[w++] = v[j][q];
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
