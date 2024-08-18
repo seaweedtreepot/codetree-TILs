b1e83fc67ce9e1716844b7162199f6d2b50c70bb
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-08-18 23:18:35 +0900

    Title: 최고의 33위치, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240818/최고의 33위치/best-place-of-33.cpp
+++ 240818/최고의 33위치/best-place-of-33.cpp
@@ -17,12 +17,12 @@ int main() {
     for(int i = 0; i <= n-3; i++){
         for(int j = 0; j <= n-3; j++){
             cnt += arr[i][j] + arr[i][j+1] + arr[i][j+2] 
-                    + arr[i+1][j] + arr[i+1][j+1] + arr[i+2][j+2]
+                    + arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2]
                     + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
             if(cnt > max){
                 max = cnt;
-                cnt=0;
             }
+            cnt=0;
         }
         
         
