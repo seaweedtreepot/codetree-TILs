f52181e2a0bb9baa8ff367592978b30eb95a6658
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-09-07 12:13:26 +0900

    Title: 비교 연산, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240907/비교 연산/comparison-operator.py
@@ -0,0 +1,12 @@
+a = input()
+
+a = a.split()
+
+a,b = int(a[0]),int(a[1])
+
+print(1 if a>=b else 0)
+print(1 if a>b else 0)
+print(1 if b>=a else 0)
+print(1 if b>a else 0)
+print(1 if a==b else 0)
+print(1 if a!=b else 0)
